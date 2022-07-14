5d1e922944833c1ca3c42d44996d7c909a3c9cb4
keviin00 <90279937+keviin00@users.noreply.github.com>
2022-07-14 11:11:43 -0300

    Add files via upload

---
+++ NumerosFloat/src/module-info.java
@@ -0,0 +1,2 @@
+module PrimerDesafioCoderHouse {
+}
---
+++ NumerosFloat/src/primerDesafioCodehouse/NumerosFlotantesAyala.java
@@ -0,0 +1,13 @@
+package primerDesafioCodehouse;
+
+public class NumerosFlotantesAyala {
+		public static void main(String[] args) {
+		
+			float numero1=3.14f;
+			float numero2=94.689435f;
+			
+			System.out.print(numero1 + numero2);
+		}
+	
+
+}
